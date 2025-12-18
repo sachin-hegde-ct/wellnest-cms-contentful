387c515b031a3aa54a31124c533b181a72276053
Sachin Hegde <sachin.hegde@codeandtheory.com>
2025-12-18 10:01:43 +0530

    Project code cleanup for migration framework setup

--- src/scripts/env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID!;
-export const CONTENTFUL_ENVIRONMENT =
-  process.env.CONTENTFUL_ENVIRONMENT || "master";
-export const CONTENTFUL_MANAGEMENT_TOKEN =
-  process.env.CONTENTFUL_MANAGEMENT_TOKEN!;
